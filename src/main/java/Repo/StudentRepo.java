0bae5dfae5852c187e11aa605065af0367cbb5d6
pratikshyaadhikari <P.Adhikari9755@student.leedsbeckett.ac.uk>
2023-04-28 16:22:23 +0100

    added assorted beans and runable register and login page completed

--- src/main/java/Repo/StudentRepo.java
+++
@@ -1,8 +0,0 @@
-package Repo;
-
-import com.pratikshya.StudentPortal.model.StudentAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepo extends JpaRepository<StudentAccount, Long> {
-
-}
